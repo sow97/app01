879b181a183d020cddf9a0d6543070a638f67881
testnexwave@outlook.com <testnexwave@outlook.com>
2019-05-27 14:08:59 -0700

    commitall

---
+++ ConsoleApp/Prgrm2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp
+{
+    class Prgrm2
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("http://localhost/CapgeminiWebApp01/StateMangement/QueryStringDemo/ex01.aspx");
+            IWebElement element1 = chrome.FindElement(By.Name("TextBox1"));
+            element1.SendKeys("sowmya");
+            IWebElement button1 = chrome.FindElement(By.Name("Button1"));
+            button1.Click();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver chrome = new ChromeDriver();
+            chrome.Navigate().GoToUrl("http://www.google.com");
+            IWebElement element = chrome.FindElement(By.Name("q"));
+            element.SendKeys("Gym Near To Me");
+            element.SendKeys(Keys.Enter);
+            Console.ReadKey();
+         
+
+        }
+    }
+}
